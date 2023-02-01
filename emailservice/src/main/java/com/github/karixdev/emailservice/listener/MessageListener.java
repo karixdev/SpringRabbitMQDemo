fb79aaf7ff1874b0597db60025a04157f96c007a
karixdev <30869888+karixdev@users.noreply.github.com>
2023-02-01 18:52:26 +0100

    feat: send email after mq message received

--- emailservice/src/main/java/com/github/karixdev/emailservice/listener/MessageListener.java
+++ emailservice/src/main/java/com/github/karixdev/emailservice/listener/MessageListener.java
@@ -2,13 +2,23 @@ package com.github.karixdev.emailservice.listener;
 
 import com.github.karixdev.emailservice.config.MQConfig;
 import com.github.karixdev.emailservice.dto.EmailResponse;
+import com.github.karixdev.emailservice.service.EmailService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class MessageListener {
+    private final EmailService emailService;
+
     @RabbitListener(queues = MQConfig.EMAIL_QUEUE)
     private void listen(EmailResponse message) {
-        System.out.println(message);
+        emailService.send(
+                "email-service@company.com",
+                message.recipientEmail(),
+                message.topic(),
+                message.body()
+        );
     }
 }
